f4621d1342951cd5941ffd74a6371ecc91e08db7
200335297 <Gowtham.Talluri@MyGeorgian.ca>
2017-01-23 18:30:37 -0500

    Changed the panel name

--- SalesBouns/Form1.Designer.cs
+++ SalesBouns/Form1.Designer.cs
@@ -223,7 +223,7 @@
             this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sales Bouns";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
